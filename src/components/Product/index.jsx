1824495ca9a800439b678fbbd362d70dec3b85a6
sergio arenas <sergioarenas025@gmail.com>
2022-06-27 10:52:19 -0500

    feat: working with the API

---
+++ src/components/Product/index.jsx
@@ -0,0 +1,14 @@
+import getData from "../../utils/api"
+
+const Product = () => {
+    
+    getData()
+    
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Product
