96b1e0a4d0d7c28481648f7b9ad138d582c21703
sergio arenas <sergioarenas025@gmail.com>
2022-07-11 12:36:48 -0500

    feat: already working te useContext, sharing the value among the components

--- App.jsx
+++ App.jsx
@@ -1,12 +1,20 @@
 import Layout from './src/components/Layout'
 import Home from './src/components/Home'
+import Context from './src/context/Context'
+import {} from 'react'
 import './App.scss'
+import useInitialState from './src/hooks/useInitialState'
 
 const App = () => {
+
+    const initialState = useInitialState()
+
     return(
-        <Layout>
-            <Home/>
-        </Layout>
+        <Context.Provider value={initialState}>
+            <Layout>
+                <Home/>
+            </Layout>
+        </Context.Provider>
     )
 }
 
